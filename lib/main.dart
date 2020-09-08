f9320e1f50d7509260f20a2dce854787e3e98a8b
Appal <appala@easycloud.in>
2020-09-08 17:50:43 +0530

    Fix image overflow

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home:Home()
-));
-
+void main() => runApp(MaterialApp(home: Home()));
 
 class Home extends StatelessWidget {
   @override
@@ -19,35 +16,39 @@ class Home extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-         Padding(
-           padding: const EdgeInsets.all(8.0),
-           child: Expanded(child: Image.asset('Assets/93aa442b744a965ca0a2d21efc8560ef.jpg'),
-           flex: 3
-           ),
-         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Expanded(child: Image.asset('Assets/d31b3ee013f9f6b692f359d2478bb802.jpg'),
-           flex: 3
-           ),
-        ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Expanded(child: Image.asset('Assets/f03de1a604d540a1a6e9c03a0d4274ee.jpg'),
-         flex: 3
-         ),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('Assets/93aa442b744a965ca0a2d21efc8560ef.jpg'),
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Expanded(child: Image.asset('Assets/Game-PS4-Advanced-Suit-Costume-Halloween-3D-printed-Cosplay-Flexible-JumpSuit-For-Kid-Adult-Men.jpg_q50.jpg'),
-         flex: 3
-         ),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('Assets/d31b3ee013f9f6b692f359d2478bb802.jpg'),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('Assets/f03de1a604d540a1a6e9c03a0d4274ee.jpg'),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                    'Assets/Game-PS4-Advanced-Suit-Costume-Halloween-3D-printed-Cosplay-Flexible-JumpSuit-For-Kid-Adult-Men.jpg_q50.jpg'),
+            ),
           ),
         ],
       ),
-
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {},
         child: Icon(Icons.edit),
         backgroundColor: Colors.red[400],
       ),
